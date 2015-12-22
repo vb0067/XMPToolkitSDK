34186cc1b517bbc93a828cbe41b709cfc08d13fe
yu <yu@yyx-me.com>
2015-12-22 11:50:46 +0800

    Initial Commit

---
+++ XMPToolkitSDK/XMPToolkitSDK.h
@@ -0,0 +1,13 @@
+//
+//  XMPToolkitSDK.h
+//  XMPToolkitSDK
+//
+//  Created by yu on 15/12/22.
+//  Copyright © 2015年 xml. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMPToolkitSDK : NSObject
+
+@end
---
+++ XMPToolkitSDK/XMPToolkitSDK.m
@@ -0,0 +1,13 @@
+//
+//  XMPToolkitSDK.m
+//  XMPToolkitSDK
+//
+//  Created by yu on 15/12/22.
+//  Copyright © 2015年 xml. All rights reserved.
+//
+
+#import "XMPToolkitSDK.h"
+
+@implementation XMPToolkitSDK
+
+@end
